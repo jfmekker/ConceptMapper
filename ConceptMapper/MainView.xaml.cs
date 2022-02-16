2ac0e59df004b0c3b74e4413c781121b082c5ef5
jfmekker <main@jacobmekker.com>
2022-02-16 12:11:59 -0800

    Add project files.

---
+++ ConceptMapper/MainView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace ConceptMapper
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainView : Window
+	{
+		public MainView( )
+		{
+			this.DataContext = new MainViewModel( );
+			this.InitializeComponent( );
+		}
+	}
+}
---
+++ ConceptMapper/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceptMapper
+{
+	public class MainViewModel
+	{
+		public string Depth => "Hello!";
+	}
+}
